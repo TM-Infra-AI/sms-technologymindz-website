2111f30ece9763f57094d2c6673ffbe9314081b6
TM-Infra-AI <infra@technologymindz.com>
2026-04-15 15:16:45 +0530

    Add files via upload

---
+++ update_twilio_account.php
@@ -0,0 +1,40 @@
+<?php
+
+
+/**
+
+
+ * @Author: Sanjay bhatt
+
+
+ * @Date:   2016-02-15 14:05:55
+
+
+ * @Last Modified by:   Nadim Ahmad
+
+
+ * @Last Modified time: 2016-02-12 18:18:40
+
+
+ */
+
+
+ob_start();
+session_start();
+include("connection.php");
+
+ $service_type =  $_POST['service_type'] ;
+ $sms_per_minute = $_POST['sms_per_minute'];
+ $sid =  $_POST['sid'] ;
+$token =  $_POST['token'] ;
+
+$sql=mysqli_query($con,"update tapp_twilio_account set service_type='".$service_type."',twilio_sid='".$sid."',twilio_token='".$token."',sms_per_minute='".$sms_per_minute."' where id='".$_POST['id']."'");
+ 
+if (!$sql) {
+$_SESSION['failure_message'] = 'Sorry! Something went wrong.';
+  } 
+  else{
+$_SESSION['flash_message'] = 'Account has been updated successfully.';
+}
+header("Location:add_plivo_account.php?s=3");
+?>
